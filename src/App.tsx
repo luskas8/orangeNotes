606b88423b2591649e7d832c3111efc5be7fd3b0
luskas8 <gnomo1049@gmail.com>
2022-09-04 16:45:56 -0300

    [FIX] vercel config

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,27 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { routes } from "@config/routes.config";
 import { AccountProvider, BreakpointProvider, FirebaseProvider, LeavingGuardProvider, NavigationProvider } from "@contexts";
-import { NavItensProps } from "@types";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Platform } from "./layouts/Platform";
 import './locates';
 import { theme } from "./theme";
 
 function App() {
     return (
-        <Router>
-            <ChakraProvider theme={theme}>
-                <LeavingGuardProvider>
-                    <AccountProvider>
-                        <FirebaseProvider>
-                            <BreakpointProvider>
+        <ChakraProvider theme={theme}>
+            <LeavingGuardProvider>
+                <AccountProvider>
+                    <FirebaseProvider>
+                        <BreakpointProvider>
+                            <Router>
                                 <NavigationProvider>
-                                    <Routes>
-                                        {routes.map(createRoute)}
-                                    </Routes>
                                     <Platform />
                                 </NavigationProvider>
-                            </BreakpointProvider>
-                        </FirebaseProvider>
-                    </AccountProvider>
-                </LeavingGuardProvider>
-            </ChakraProvider>
-        </Router>
-    )
-}
-
-function createRoute(route: NavItensProps) {
-    return (
-        <Route key={route.path} path={route.path} element={<route.component {...route} />}>
-            {route.routes?.map(createRoute)}
-        </Route>
+                            </Router>
+                        </BreakpointProvider>
+                    </FirebaseProvider>
+                </AccountProvider>
+            </LeavingGuardProvider>
+        </ChakraProvider>
     )
 }
 
